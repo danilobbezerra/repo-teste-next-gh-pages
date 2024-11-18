5361794b4702ce82861ae0e6264897bcd88a9396
Danilo <danilobarretobezerra@gmail.com>
2024-11-18 00:35:43 -0300

    subindo gh pages

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+const nextConfig = {
+  output: 'export',
+  basePath: '/repo-teste-next-gh-pages', // Substitua pelo nome do repositório no GitHub
+  trailingSlash: true, // Adiciona barra no final das URLs para melhor compatibilidade
+  
+};
+
+module.exports = nextConfig;
