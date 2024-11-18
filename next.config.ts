40ae52c777f1c3141b76ec61eaf321a9cad0669c
Danilo <danilobarretobezerra@gmail.com>
2024-11-18 00:43:46 -0300

    ajuste

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-const nextConfig = {
-  output: 'export',
-  basePath: '/repo-teste-next-gh-pages', // Substitua pelo nome do repositório no GitHub
-  trailingSlash: true, // Adiciona barra no final das URLs para melhor compatibilidade
-  
-};
-
-module.exports = nextConfig;
